b71706616bb6dc686e3eb86d99a8256a743267c0
midnightpicnic <midnightpicnicstudio@gmail.com>
2022-01-18 12:45:43 -0800

    analytics

--- wp-content/themes/genesis/lib/views/help/genesis-inpost-seo.php
+++ wp-content/themes/genesis/lib/views/help/genesis-inpost-seo.php
@@ -17,7 +17,7 @@
 </tr>
 <tr>
 <td bgcolor="#eeeeec" align="center">1</td>
-<td bgcolor="#eeeeec" align="center">0.0000</td>
+<td bgcolor="#eeeeec" align="center">0.0001</td>
 <td bgcolor="#eeeeec" align="right">405416</td>
 <td bgcolor="#eeeeec">{main}(  )</td>
 <td title="/Users/sb/Local Sites/personal-midnight-picnic/app/public/wp-content/themes/genesis/lib/views/help/genesis-inpost-seo.php" bgcolor="#eeeeec">.../genesis-inpost-seo.php<b>:</b>0</td>
